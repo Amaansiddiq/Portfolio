bffcea6c1b864f35ed848879756fc716485dfb85
Amaan <amaansiddiq786@gmail.com>
2025-03-08 23:14:04 +0530

    Updated Script and Media

--- script.js
+++ script.js
@@ -6,19 +6,3 @@ function toggleMenu() {
 }
 
 
-function toggleTheme() {
-  const body = document.body;
-  const themeToggleButton = document.getElementById("theme-toggle-btn");
-
-  // Toggle the class on the body element to switch between themes
-  body.classList.toggle("dark-mode");
-
-  // Change the icon based on the current theme
-  if (body.classList.contains("dark-mode")) {
-    themeToggleButton.innerHTML = '<i class="fas fa-moon"></i>'; // Dark mode icon
-    localStorage.setItem("theme", "dark");
-  } else {
-    themeToggleButton.innerHTML = '<i class="fas fa-sun"></i>'; // Light mode icon
-    localStorage.setItem("theme", "light");
-  }
-}
